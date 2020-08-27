7e77d30bf3861f526e730e3328904514572dff0b
nigel5 <28766663+nigel5@users.noreply.github.com>
2020-08-26 21:30:43 -0400

    feat: create and decode urls

--- middleware/api.js
+++ middleware/api.js
@@ -1,11 +1,11 @@
-const { Client } = require("cassandra-driver");
+const { Client } = require('cassandra-driver');
 
 /**
  * Public API for One Click Shorten
  * @param {Client} cassandraClient Client to execute commands on
  */
 module.exports = function (cassandraClient) {
-  const router = require("express").Router();
+  const router = require('express').Router();
   const LocalDate = require('cassandra-driver').types.LocalDate;
 
   const responses = require('../util/responses');
@@ -13,12 +13,16 @@ module.exports = function (cassandraClient) {
   const settings = require('../ocshorten.conf.json');
   const statements = require('../util/database/statements');
 
-  const numberOfCharacters = process.env.GENERATED_URL_LENGTH ? process.env.GENERATED_URL_LENGTH : settings.generated_url_length;
-  const baseURL = process.env.BASE_URL ? process.env.BASE_URL : settings.base_url;
+  const numberOfCharacters = process.env.GENERATED_URL_LENGTH
+    ? process.env.GENERATED_URL_LENGTH
+    : settings.generated_url_length;
+  const baseURL = process.env.BASE_URL
+    ? process.env.BASE_URL
+    : settings.base_url;
 
   /**
    * Get a short url
-   * 
+   *
    * Query parameters
    * q string The original url
    */
@@ -30,30 +34,72 @@ module.exports = function (cassandraClient) {
     const originalUrl = req.query.q;
 
     if (!originalUrl) {
-      return res.send(responses.errResponse(false, "You must provide a url to shorten"));
+      return res.send(
+        responses.errResponse(false, 'You must provide a url to shorten')
+      );
     }
 
     const shortUrl = `${baseURL}/${letters}`;
     const requesterIp = req.ip;
 
-    res.send(responses.dataResponse({
-      url: shortUrl
-    }));
+    res.send(
+      responses.dataResponse({
+        url: shortUrl,
+      })
+    );
 
     // Save url in cache
 
     // Save in perm database for long term
-    cassandraClient.execute(statements.INSERT_URL, [originalUrl, shortUrl, LocalDate.now(), requesterIp]);
+    cassandraClient.execute(statements.INSERT_URL, [
+      originalUrl,
+      shortUrl,
+      LocalDate.now(),
+      requesterIp,
+    ]);
   });
 
   /**
    * Decodes a short url
-   * 
+   *
    * Query parameters
    * q string The shortened url
    */
   router.get('/api/v1/decode', async function (req, res, next) {
+    const shortenedUrl = req.query.q;
+
+    if (!shortenedUrl) {
+      return res.send(
+        responses.errResponse(
+          false,
+          'You must provide a shortened url to decode'
+        )
+      );
+    }
+
+    try {
+      let result = await cassandraClient.execute(statements.SELECT_URL_2, [
+        shortenedUrl,
+      ]);
+
+      if (result.rowLength < 1) {
+        return res.send(
+          responses.errResponse(false, `${shortenedUrl} does not exist`)
+        );
+      }
+
+      result = result.first();
+
+      return res.send(
+        responses.dataResponse({
+          fromUrl: result.from_url,
+          toUrl: result.to_url,
+        })
+      );
+    } catch (e) {
+      console.log(e);
+    }
   });
 
   return router;
-}
+};
--- util/database/statements.js
+++ util/database/statements.js
@@ -1,9 +1,17 @@
 /**
  * Insert a mapped url
  */
-module.exports.INSERT_URL = "INSERT INTO url_mapping (from_url, to_url, created_on, original_creator_ip) VALUES (?, ?, ?, ?)";
+module.exports.INSERT_URL =
+  'INSERT INTO url_mapping (from_url, to_url, created_on, original_creator_ip) VALUES (?, ?, ?, ?)';
 
 /**
- * Retrieve a mapped url
+ * Retrieve a mapped url via from_url
  */
-module.exports.SELECT_URL = "SELECT from_url, to_url FROM url_mapping WHERE from_url = ?";
+module.exports.SELECT_URL_1 =
+  'SELECT from_url, to_url FROM url_mapping WHERE from_url = ?';
+
+/**
+ * Retrieve a mapped url via to_url
+ */
+module.exports.SELECT_URL_2 =
+  'SELECT from_url, to_url FROM url_mapping WHERE to_url = ?';
