c08a939ff12df378544a70af63da1e884a11b3ed
nigel5 <28766663+nigel5@users.noreply.github.com>
2020-09-02 00:41:28 -0400

    fix: data persistance bug

--- scripts/cassandra-init.sh
+++ scripts/cassandra-init.sh
@@ -1,5 +1,4 @@
-CQL="DROP KEYSPACE ocurl;
-CREATE KEYSPACE ocurl WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'} AND durable_writes = true;
+CQL="CREATE KEYSPACE IF NOT EXISTS ocurl WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'} AND durable_writes = true;
 CREATE TABLE IF NOT EXISTS ocurl.url_mapping (from_key varchar, to_url varchar, created_on date, original_creator_ip varchar, PRIMARY KEY (from_key));
 CREATE INDEX IF NOT EXISTS ON ocurl.url_mapping (to_url);
 USE ocurl;"
